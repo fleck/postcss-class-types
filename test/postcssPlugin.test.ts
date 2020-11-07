79fb641b25f87afd5b1fb361c2d3b5dedccaa609
Jonathan <jonfleck88@gmail.com>
2020-11-07 07:48:44 -0500

    setup failling test

--- test/postcssPlugin.test.ts
+++ test/postcssPlugin.test.ts
@@ -48,27 +48,33 @@ test("supports custom directories", async () => {
 });
 
 test("groups", async () => {
-  const directory = path.join(typesDirectory, "custom");
+  await run("#td .group:hover .group-hover\\:text-gray-900{}", {});
 
-  await run("#td .group:hover .group-hover\\:text-gray-900{}", { directory });
-
-  expect(
-    (await fs.readFile(path.join(directory, "classNames.d.ts"))).toString()
-  ).toMatchSnapshot();
+  expect((await fs.readFile(classNamesPath)).toString()).toMatchSnapshot();
 });
 
 test("space", async () => {
-  const directory = path.join(typesDirectory, "custom");
-
   await run(
     `.space-y-2>:not(template)~:not(template) {
        --space-y-reverse:0!important;margin-top: calc(0.5rem*(1 - var(--space-y-reverse)))!important;
        margin-bottom: calc(0.5rem*var(--space-y-reverse))!important
      }`,
-    { directory }
+    {}
   );
 
-  expect(
-    (await fs.readFile(path.join(directory, "classNames.d.ts"))).toString()
-  ).toMatchSnapshot();
+  expect((await fs.readFile(classNamesPath)).toString()).toMatchSnapshot();
+});
+
+test("comma", async () => {
+  await run(
+    `button,input,optgroup,select,textarea {
+       font-family: inherit;
+       font-size: 100%;
+       line-height: 1.15;
+       margin: 0
+     }`,
+    {}
+  );
+
+  expect((await fs.readFile(classNamesPath)).toString()).toMatchSnapshot();
 });
